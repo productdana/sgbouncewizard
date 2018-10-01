75e7f980e2e5178f9e0d2397223f733c906318cf
Dana Tran <hellodana.me@gmail.com>
2018-10-01 16:44:15 -0700

    added sass support

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const devMode = process.env.NODE_ENV !== "production";
 
 module.exports = {
   entry: {
@@ -21,12 +23,29 @@ module.exports = {
         use: {
           loader: "babel-loader"
         }
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        exclude: /node_modules/,
+        use: [
+          // Style-loader fallback adds CSS to DOM by injecting a <style> tag in dev mode
+          // MiniCSSExtract plugin creates a CSS file per JS file which contains CSS in production mode
+          devMode ? "style-loader" : MiniCssExtractPlugin.loader,
+          // Interprets imports/requires and resolves them for .css files
+          "css-loader",
+          // First converts .scss/.sass files into .css files
+          "sass-loader"
+        ]
       }
     ]
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/index.html"
+    }),
+    new MiniCssExtractPlugin({
+      filename: devMode ? "[name].css" : "[name].[contenthash].css",
+      chunkFilename: devMode ? "[id].css" : "[id].[contenthash].css"
     })
   ],
   devServer: {
