5fafe3cb0c8f484fd2f843e1fc61e22ce7defc23
Dana Tran <hellodana.me@gmail.com>
2018-10-05 14:10:36 -0700

    set up react router and sample route

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { Card } from "@sendgrid/ui-components";
 import HelloWorld from "./components/HelloWorld";
 import "./index.scss";
+
+const TestComponent = () => <div>Test!</div>;
 
 const App = () => (
   <div>
@@ -10,9 +13,17 @@ const App = () => (
     <div style={{ width: "300px" }}>
       <Card title="SG Bounce Wizard" body="Manage your bounce rules" />
     </div>
+    <Switch>
+      <Route path="/test" exact component={TestComponent} />
+    </Switch>
   </div>
 );
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
 
 export default App;
