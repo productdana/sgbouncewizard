b8627bbd81941007e143306dcfc3e7f242ced2ce
Khuong Tiet <khuongtiet@dhcp-v112-141.mobile.uci.edu>
2018-10-11 12:30:20 -0700

    added pagination and pulling from db

--- src/components/BounceRules/index.jsx
+++ src/components/BounceRules/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Redirect } from "react-router-dom";
 import "./BounceRules.css";
+import axios from "axios";
 
 export default class BounceRules extends React.Component {
   constructor(props) {
@@ -10,10 +11,26 @@ export default class BounceRules extends React.Component {
       searchToken: "",
       isRedirectingToDetail: false,
       selectedRule: {},
+      rules: [],
+      pageIndex: 1,
+      pageInterval: 10,
     };
 
     this.updateSearchToken = this.updateSearchToken.bind(this);
     this.handleKeyDown = this.handleKeyDown.bind(this);
+  }
+
+  async componentDidMount() {
+    const { data, status } = await axios.get(
+      "http://localhost:3000/bounce_rules"
+    );
+    if (status === 200) {
+      const { rules, numRules } = data;
+      this.setState({
+        rules,
+        numRules,
+      });
+    }
   }
 
   handleRuleClick(rule) {
@@ -41,61 +58,35 @@ export default class BounceRules extends React.Component {
 
     return rules.filter(
       rule =>
-        rule.action.toLowerCase().includes(searchToken) ||
-        rule.description.toLowerCase().includes(searchToken) ||
-        rule.lastCommit.toLowerCase().includes(searchToken)
+        rule.bounce_action.toLowerCase().includes(searchToken) ||
+        rule.description.toLowerCase().includes(searchToken)
     );
   }
 
+  paginate(rules) {
+    const { pageIndex, pageInterval } = this.state;
+    return rules.slice(pageIndex - 1, pageIndex - 1 + pageInterval);
+  }
+
+  updatePageIndex(newIndex) {
+    this.setState(prevState => ({
+      pageIndex:
+        prevState.pageIndex !== newIndex ? newIndex : prevState.pageIndex,
+    }));
+  }
+
   render() {
-    const { isRedirectingToDetail, selectedRule } = this.state;
+    const {
+      isRedirectingToDetail,
+      pageIndex,
+      selectedRule,
+      rules,
+      numRules,
+    } = this.state;
+    const filteredRules = this.filterRules(this.paginate(rules));
 
-    const proxyBounceRules = [
-      {
-        action: "Bounce1 Action",
-        description: "Server Timeout",
-        lastCommit: "N/A",
-        id: 1,
-      },
-      {
-        action: "Bounce2 Action",
-        description: "Invalid IP",
-        lastCommit: "N/A",
-        id: 2,
-      },
-      {
-        action: "Bounce3 Action",
-        description: "Other",
-        lastCommit: "N/A",
-        id: 3,
-      },
-      {
-        action: "Bounce4 Action",
-        description: `Lorem ipsum dolor sit amet, 
-          consectetur adipiscing elit, 
-          sed do eiusmod tempor incididunt 
-          ut labore et dolore magna aliqua. 
-          Ut enim ad minim veniam, quis nostrud 
-          exercitation ullamco laboris nisi ut 
-          aliquip ex ea commodo consequat. 
-          Duis aute irure dolor in reprehenderit 
-          in voluptate velit esse cillum dolore 
-          eu fugiat nulla pariatur. Excepteur 
-          sint occaecat cupidatat non proident, 
-          sunt in culpa qui officia deserunt 
-          mollit anim id est laborum.`,
-        lastCommit: "N/A",
-        id: 4,
-      },
-      {
-        action: "Bounce5 Action",
-        description: "Other",
-        lastCommit: "N/A",
-        id: 5,
-      },
-    ];
-
-    const filteredRules = this.filterRules(proxyBounceRules);
+    const endPage = pageIndex + (5 - (pageIndex % 5 === 0 ? 5 : pageIndex % 5));
+    const startPage = endPage - 4;
 
     return isRedirectingToDetail ? (
       <Redirect
@@ -154,25 +145,87 @@ export default class BounceRules extends React.Component {
               Create a Bounce Rule
             </button>
           </div>
-          {filteredRules.map(rule => (
-            <div
-              key={rule.id}
-              className="bounceRules-ruleContainer"
-              style={{
-                border: "0.125rem solid grey",
-                padding: "2rem",
-                margin: "3rem 0",
-              }}
-              onClick={() => this.handleRuleClick(rule)}
-              onKeyDown={() => this.handleKeyDown(rule)}
-              role="button"
-              tabIndex="0"
-            >
-              <div>Bounce Action: {rule.action}</div>
-              <div>Description: {rule.description}</div>
-              <div>Last Commit Message: {rule.lastCommit}</div>
+          <div>
+            {filteredRules.map(rule => (
+              <div
+                key={rule.id}
+                className="bounceRules-ruleContainer"
+                style={{
+                  border: "0.125rem solid grey",
+                  padding: "2rem",
+                  margin: "3rem 0",
+                }}
+                onClick={() => this.handleRuleClick(rule)}
+                onKeyDown={() => this.handleKeyDown(rule)}
+                role="button"
+                tabIndex="0"
+              >
+                <div>Bounce Action: {rule.bounce_action}</div>
+                <div style={{ wordWrap: "break-word" }}>
+                  Description: {rule.description}
+                </div>
+                <div>Last Commit Message: </div>
+              </div>
+            ))}
+            <div className="pagination">
+              <a
+                className="btn btn-secondary btn-small pagination-prev"
+                onClick={() =>
+                  this.setState(prevState => ({
+                    pageIndex:
+                      prevState.pageIndex > 1 ? prevState.pageIndex - 1 : 0,
+                  }))
+                }
+                onKeyDown={() => {}}
+                role="button"
+                tabIndex="0"
+              >
+                Prev
+              </a>
+              <div className="pagination-links">
+                {Array(endPage - startPage + 1)
+                  .fill()
+                  .map((_, i) => startPage + i)
+                  .map(number => (
+                    <a
+                      key={number}
+                      className={`pagination-link ${
+                        number === pageIndex ? " is-active" : ""
+                      }`}
+                      onClick={() => this.updatePageIndex(number)}
+                      onKeyDown={() => {}}
+                      role="button"
+                      tabIndex="0"
+                    >
+                      {number}
+                    </a>
+                  ))}
+                <a className="pagination-ellipses">&hellip;</a>
+                <a
+                  className="pagination-link"
+                  onClick={() => this.updatePageIndex(numRules)}
+                  onKeyDown={() => {}}
+                  role="button"
+                  tabIndex="0"
+                >
+                  {numRules}
+                </a>
+              </div>
+              <a
+                className="btn btn-secondary btn-small pagination-next"
+                onClick={() =>
+                  this.setState(prevState => ({
+                    pageIndex: prevState.pageIndex + 1,
+                  }))
+                }
+                onKeyDown={() => {}}
+                role="button"
+                tabIndex="0"
+              >
+                Next
+              </a>
             </div>
-          ))}
+          </div>
         </div>
       </div>
     );
