524b974d1d612052fea1f9e71e0acaa9bd6cc7fc
Kenny Matsudo <kennymatsudo@live.com>
2019-01-09 19:43:07 -0800

    changed no rules description

--- src/components/BounceRulesContainer/EmptyRules/index.jsx
+++ src/components/BounceRulesContainer/EmptyRules/index.jsx
@@ -3,10 +3,7 @@ import EmptyState from "@sendgrid/ui-components/empty-state";
 
 const EmptyRules = () => (
   <EmptyState icon="warning-triangle" header="No rules available.">
-    <p>
-      No rules are currently available to display. Please try reloading or
-      contact an administrator if the problem persists.
-    </p>
+    <p>No rules are currently available to display.</p>
   </EmptyState>
 );
 
