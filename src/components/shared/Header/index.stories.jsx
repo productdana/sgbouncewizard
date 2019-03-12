58e3d299efefdbed47f38fca4e0818a7670f3e6f
Kenny Matsudo <kennymatsudo@live.com>
2019-03-11 20:13:56 -0700

    Storybook v5 (#64)
    * storybook5

--- src/components/shared/Header/index.stories.jsx
+++ src/components/shared/Header/index.stories.jsx
@@ -2,6 +2,4 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import Header from ".";
 
-storiesOf("Header", module)
-  .add("Default", () => <Header name="Kenny" />)
-  .add("No Name", () => <Header />);
+storiesOf("Header", module).add("Default", () => <Header />);
